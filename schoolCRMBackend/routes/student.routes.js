d46a67ee3cf46119a54444dee37d6dc6050da481
Subham Raj <rajshubham199@gmail.com>
2024-12-11 02:50:22 +0530

    some changes

--- schoolCRMBackend/routes/student.routes.js
+++ schoolCRMBackend/routes/student.routes.js
@@ -1,17 +1,17 @@
 import express from 'express';
 
 import { addStudent, getStudents, updateStudent, deleteStudent,signupStudent, loginStudent, getStudentByEmail } from '../controllers/student.controller.js';
-import { verifyToken } from '../authMiddleware.js';
+// import { verifyToken } from '../authMiddleware.js';
 
 const router = express.Router();
 
-router.post('/add', verifyToken, addStudent);
-router.get('/display', verifyToken, getStudents);
-router.put('/update/:_id', verifyToken, updateStudent);
-router.delete('/delete/:_id', verifyToken, deleteStudent);
+router.post('/add',  addStudent);
+router.get('/display',  getStudents);
+router.put('/update/:_id',  updateStudent);
+router.delete('/delete/:_id',  deleteStudent);
 
 router.post('/signup', signupStudent); // Student signup
 router.post('/login', loginStudent);   // Student login
-router.get('/find/:email', verifyToken, getStudentByEmail)
+router.get('/find/:email',  getStudentByEmail)
 
 export default router;
--- schoolCRMBackend/routes/teacher.routes.js
+++ schoolCRMBackend/routes/teacher.routes.js
@@ -1,14 +1,14 @@
 import express from 'express';
 import { addTeacher, getTeachers, updateTeacher, deleteTeacher, signupTeacher, loginTeacher, getTeacherById } from '../controllers/teacher.controller.js';
-import { verifyToken } from '../authMiddleware.js';
+// import { verifyToken } from '../authMiddleware.js';
 
 const router = express.Router();
 
-router.post('/add', verifyToken, addTeacher);
-router.get('/display', verifyToken, getTeachers);
-router.put('/update/:_id', verifyToken, updateTeacher);
-router.delete('/delete/:_id', verifyToken, deleteTeacher);
-router.get('/find/:_id',verifyToken, getTeacherById);
+router.post('/add',  addTeacher);
+router.get('/display',  getTeachers);
+router.put('/update/:_id',  updateTeacher);
+router.delete('/delete/:_id',  deleteTeacher);
+router.get('/find/:_id', getTeacherById);
 
 router.post('/signup', signupTeacher);
 router.post('/login', loginTeacher);
